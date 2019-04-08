b5aa962971e6f7f932db81728b5891d77fb3e77c
Tomasz Strzoda <strzoda.tomasz@gmail.com>
2019-04-08 22:19:32 +0200

    Add about window

---
+++ src/gui/controllers/AboutController.java
@@ -0,0 +1,45 @@
+package gui.controllers;
+
+import gui.MainWindow;
+import javafx.application.Platform;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class AboutController implements Initializable {
+
+    @FXML private ImageView closeApp;
+    @FXML private ImageView minimizeApp;
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+    }
+
+    /*
+    @FXML
+    void navBarAction(MouseEvent e) throws IOException {
+        if (e.getSource().equals(closeApp))
+            Platform.exit();
+        else if (e.getSource().equals(minimizeApp)) {
+            MainWindow mainWindow = new MainWindow();
+            mainWindow.getStage().setIconified(true);
+        }
+    }
+
+    @FXML
+    void transparentOn(MouseEvent e) {
+        if      (e.getSource().equals(closeApp))        closeApp.setOpacity(0.5);
+        else if (e.getSource().equals(minimizeApp))     minimizeApp.setOpacity(0.5);
+    }
+
+    @FXML
+    void transparentOff(MouseEvent e) {
+        if      (e.getSource().equals(closeApp))        closeApp.setOpacity(1);
+        else if (e.getSource().equals(minimizeApp))     minimizeApp.setOpacity(1);
+    }*/
+}
--- src/gui/controllers/HomePageController.java
+++ src/gui/controllers/HomePageController.java
@@ -59,15 +59,34 @@ public class HomePageController implements Initializable {
     }
 
     @FXML
-    void navBarAction(MouseEvent e) {
+    void navBarAction(MouseEvent e) throws IOException {
         if (e.getSource().equals(closeApp))
             Platform.exit();
         else if (e.getSource().equals(minimizeApp)) {
             MainWindow mainWindow = new MainWindow();
             mainWindow.getStage().setIconified(true);
         }
-        else if (e.getSource().equals(aboutApp))
+        else if (e.getSource().equals(aboutApp)) {
             System.out.println("About");
+
+
+
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(getClass().getResource("/gui/FXML/about.fxml"));
+            Scene secondScene = new Scene(loader.load());
+
+            // New window (Stage)
+            Stage newWindow = new Stage();
+            newWindow.setTitle("About");
+            newWindow.setScene(secondScene);
+
+            // Set position of second window, related to primary window.
+            //newWindow.setX(primaryStage.getX() + 200);
+            //newWindow.setY(primaryStage.getY() + 100);
+
+            newWindow.show();
+
+        }
 
     }
 
