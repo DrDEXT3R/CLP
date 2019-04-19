bc9b6e8380bf025521e2465ef0d961325b9069cf
Tomasz Strzoda <strzoda.tomasz@gmail.com>
2019-04-19 22:31:27 +0200

    Update UI for map coloring problem

--- src/gui/controllers/MapColoringController.java
+++ src/gui/controllers/MapColoringController.java
@@ -1,5 +1,9 @@
 package gui.controllers;
 
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXComboBox;
+import com.jfoenix.controls.JFXTextArea;
+import com.jfoenix.controls.JFXTextField;
 import core.MapColoring;
 import gui.MainWindow;
 
@@ -8,6 +12,7 @@ import java.net.URL;
 import java.util.ArrayList;
 import java.util.ResourceBundle;
 
+import javafx.application.Platform;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
@@ -20,6 +25,11 @@ import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
 import javafx.scene.text.TextFlow;
 
@@ -30,13 +40,24 @@ import javafx.scene.text.TextFlow;
  */
 public class MapColoringController extends BasicOptions implements Initializable {
 
-    @FXML private Button addGroup;
-    @FXML private Button mapColoringSolve;
+    @FXML private ImageView closeApp;
+    @FXML private ImageView minimizeApp;
+    @FXML private ImageView aboutApp;
+    @FXML private ImageView museumShortcut;
+    @FXML private ImageView homeShortcut;
+    @FXML private ImageView einsteinShortcut;
+    @FXML private JFXButton addGroup;
+    @FXML private JFXButton mapColoringSolve;
     @FXML private Label inputRangeLabel;
     @FXML private TextFlow mapColoringTime;
-    @FXML private TextField groupTextField;
-    @FXML private TextArea allGroups;
-    @FXML private ComboBox<Integer> noOfRegionsComboBox;
+
+    @FXML private JFXTextField groupTextField;
+
+    @FXML private JFXTextArea allGroups;
+
+    @FXML private JFXComboBox<Integer> noOfRegionsComboBox;
+
+
     @FXML private TableView<String[]> mapColoringTableView;
     @FXML private TableColumn<String[], String> regionsColumn;
     @FXML private TableColumn<String[], String> colorIndexColumn;
@@ -55,14 +76,11 @@ public class MapColoringController extends BasicOptions implements Initializable
             list.add(i + 1);
         noOfRegionsComboBox.setItems(list);
 
+
+
         groups = new String();
         mapColoringIsModeled = false;
         neighboringRegions = new ArrayList<>();
-    }
-
-    @FXML
-    public void homeAction(ActionEvent event) throws IOException {
-        loadNewScene("/gui/FXML/homePage.fxml");
     }
 
     @FXML
@@ -100,8 +118,12 @@ public class MapColoringController extends BasicOptions implements Initializable
         fillTableView(solution, mapColoringTableView, mapColoringCols);
 
         // Calculation time.
-        Text time = new Text("I calculated it in: " + activeModule.getTime() + "s");
+        Text time = new Text(round(activeModule.getTime(),6) + "s");
+        time.setFont(Font.font ("Berlin Sans FB Demi", 20));
+        time.setFill(Color.valueOf("#eda647"));
         mapColoringTime.getChildren().add(time);
+
+        mapColoringSolve.setDisable(true);
     }
 
     @FXML
@@ -136,6 +158,50 @@ public class MapColoringController extends BasicOptions implements Initializable
         groups = new String();
         mapColoringIsModeled = false;
         hideUI(true);
+
+
+    }
+
+    @FXML
+    void transparentOn(MouseEvent e) {
+        if      (e.getSource().equals(closeApp))        closeApp.setOpacity(0.5);
+        else if (e.getSource().equals(minimizeApp))     minimizeApp.setOpacity(0.5);
+        else if (e.getSource().equals(aboutApp))        aboutApp.setOpacity(0.5);
+        else if (e.getSource().equals(museumShortcut))  museumShortcut.setOpacity(0.5);
+        else if (e.getSource().equals(homeShortcut))    homeShortcut.setOpacity(0.5);
+        else if (e.getSource().equals(einsteinShortcut))einsteinShortcut.setOpacity(0.5);
+    }
+
+    @FXML
+    void transparentOff(MouseEvent e) {
+        if      (e.getSource().equals(closeApp))        closeApp.setOpacity(1);
+        else if (e.getSource().equals(minimizeApp))     minimizeApp.setOpacity(1);
+        else if (e.getSource().equals(aboutApp))        aboutApp.setOpacity(1);
+        else if (e.getSource().equals(museumShortcut))  museumShortcut.setOpacity(1);
+        else if (e.getSource().equals(homeShortcut))    homeShortcut.setOpacity(1);
+        else if (e.getSource().equals(einsteinShortcut))einsteinShortcut.setOpacity(1);
+    }
+
+    @FXML
+    void navBarAction(MouseEvent e) throws IOException {
+        if (e.getSource().equals(closeApp))
+            Platform.exit();
+        else if (e.getSource().equals(minimizeApp)) {
+            MainWindow mainWindow = new MainWindow();
+            mainWindow.getStage().setIconified(true);
+        }
+        else if (e.getSource().equals(aboutApp))
+            createNewStage("/gui/FXML/about.fxml");
+    }
+
+    @FXML
+    void bottomBarAction(MouseEvent e) throws IOException {
+        if (e.getSource().equals(einsteinShortcut))
+            loadNewScene("/gui/FXML/einstein.fxml");
+        else if (e.getSource().equals(homeShortcut))
+            loadNewScene("/gui/FXML/homePage.fxml");
+        else if (e.getSource().equals(museumShortcut))
+            loadNewScene("/gui/FXML/museum.fxml");
     }
 
 }
