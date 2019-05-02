6cd88cb8baa8b67f309e38d3ac129f97f43b469a
Tomasz Strzoda <strzoda.tomasz@gmail.com>
2019-05-02 13:22:59 +0200

    Add curiosity in map coloring help

--- src/gui/controllers/MapColoringHelpController.java
+++ src/gui/controllers/MapColoringHelpController.java
@@ -1,8 +1,23 @@
 package gui.controllers;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+import java.awt.*;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+
 /**
  * FXML Controller class for map coloring help window.
  *
  * @author Tomasz Strzoda
  */
-public class MapColoringHelpController extends BasicController{ }
+public class MapColoringHelpController extends BasicController{
+
+    @FXML
+    void linkAction(ActionEvent e) throws URISyntaxException, IOException {
+        Desktop.getDesktop().browse(new URI("https://en.wikipedia.org/wiki/Four_color_theorem"));
+    }
+
+}
